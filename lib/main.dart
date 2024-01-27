2c6f514eef05c427f77218693de2def144f3ad40
PriyathamVarma <priyatham002@gmail.com>
2024-01-27 20:23:54 +0000

    CREATE : Initial Commit'

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:chat/src/screens/auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  // await dotenv.load();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // final apiKey = dotenv.get('FIREBAE_REALTIME_DATABASE_URL');
+    return MaterialApp(
+      title: 'Chat App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Container(
+          child: const Center(
+            child: SizedBox(
+              child: AuthScreen(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
